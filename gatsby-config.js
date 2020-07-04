1bb3251fb74bde505c93323ed5dbcfc1970453bb
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-07-04 14:11:15 +0300

    basic file structure and different ways of styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Bootcamp`,
+    description: `Second Gatsby project`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-postcss`,
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -31,4 +35,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,26 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import { Link } from "gatsby";
+//get accsess to bootstrap in all files
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.module.scss";
+import Footer from "./footer";
+import Menu from "./menu";
+import Button from "../components/button";
+
+export const StyledLink = styled.div`
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,31 +35,34 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <Menu />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          padding: "16px",
+          minHeight: `100vh`,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Button>
+          <StyledLink>
+            <Link to="/">Go back</Link>
+          </StyledLink>
+        </Button>
+        <main className="mane">{children}</main>
       </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
